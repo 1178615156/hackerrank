1313b315c9c39181c1b16a709581027692609bc1
117861516 <a1178615156@gmail.com>
2016-03-17 22:48:09 +0800

    tool with read scanf api

--- src/main/scala/utils/Tool.scala
+++ src/main/scala/utils/Tool.scala
@@ -1,5 +1,7 @@
 package utils
 
+import scala.collection.immutable.IndexedSeq
+
 /**
   * Created by yujieshui on 2016/3/14.
   */
@@ -13,5 +15,17 @@ object Tool {
       false
     else
       listIsSort(l.tail, l.head)
+  }
+
+  type Grid[X] = Seq[Array[X]]
+  def readGrid[R](lineNum: Int): Grid[Char] = readGrid(lineNum,_.toArray)
+
+  def readGrid[R](lineNum :Int,s: String => R ): Seq[R] ={
+    1 to lineNum map (_ => s(io.StdIn.readLine()))
+  }
+  scala.annotation.elidable
+  case class Paint[T](xx:Int,yy:Int ,value:Option[T] = None ){
+    def row = yy
+    def column = xx
   }
 }
