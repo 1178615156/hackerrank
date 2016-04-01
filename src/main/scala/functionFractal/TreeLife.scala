adfebca9b9804a733dbe01ed59a1e9420e3cbfd2
yujieshui <yujieshui@wecash.net>
2016-04-01 18:27:47 +0800

    tree life

--- src/main/scala/functionFractal/TreeLife.scala
+++ src/main/scala/functionFractal/TreeLife.scala
@@ -1,5 +1,7 @@
 package functionFractal
 
+import utils.SetInt
+
 /**
   * Created by yujieshui on 2016/3/15.
   */
@@ -8,47 +10,78 @@ object TreeLife {
 
   trait Term
 
-  case object On extends Term
+  trait CellTerm extends Term
 
-  case object Off extends Term
+  case object On extends CellTerm
+
+  case object Off extends CellTerm
 
   case class LifeTree(
-                       left: Term,
-                       node: Term,
-                       fight: Term
+                       left: Option[LifeTree] = None,
+                       node: CellTerm,
+                       right: Option[LifeTree] = None,
+                       father: Option[CellTerm] = None
                      ) extends Term
 
 
-  def parsing(list: List[Char]) = {
-    def impl(list: List[Char],
-             currentTerm: List[Term] = Nil
-            ): (Term, List[Char]) = {
-      list.head match {
-        case '(' =>
-          val (term, tail) = impl(list.tail)
-          if (tail.isEmpty)
-            (term, tail)
-          else
-            impl(tail, currentTerm :+ term)
-        case ')' =>
-          LifeTree(
-            currentTerm(0),
-            currentTerm(1),
-            currentTerm(2)
-          ) -> list.tail
-        case 'X' =>
-          impl(list.tail, currentTerm :+ On)
-        case '.' =>
-          impl(list.tail, currentTerm :+ Off)
-      }
+  def parsing(list: List[Char],
+              currentTerm: List[LifeTree] = Nil
+             ): (LifeTree, List[Char]) = {
+    list.head match {
+      case '(' =>
+        val (term, tail) = parsing(list.tail)
+        if (tail.isEmpty)
+          (term, tail)
+        else
+          parsing(tail, currentTerm :+ term)
+      case ')' =>
+        LifeTree(
+          left = Some(currentTerm(0)),
+          node = currentTerm(1).node,
+          right = Some(currentTerm(2))
+        ) -> list.tail
+      case 'X' =>
+        parsing(list.tail, currentTerm :+ LifeTree(node = On))
+      case '.' =>
+        parsing(list.tail, currentTerm :+ LifeTree(node = Off))
     }
+  }
 
-    impl(list)._1
+  def setFather(term: Term, father: Option[CellTerm] = None): LifeTree = {
+    term match {
+      case On          => LifeTree(node = On)
+      case Off         => LifeTree(node = On)
+      case x: LifeTree =>
+        x.copy(
+          left = x.left.map(setFather(_, Some(x.node))),
+          right = x.right.map(setFather(_, Some(x.node))),
+          father = father
+        )
+    }
+  }
+
+
+  def rule7708(father: CellTerm,
+               left: CellTerm,
+               self: CellTerm,
+               right: CellTerm) = {
+
   }
 
   def main(args: Array[String]) {
     def readListInt() = io.StdIn.readLine().split(" ").toList.map(_.toInt)
     val ruleNum :: Nil = readListInt()
-    val cellTreeString = io.StdIn.readLine().filter(_ != ' ')
+    val cellTreeString = io.StdIn.readLine().toList.filter(_ != ' ')
+
+    val a = parsing(cellTreeString)
+
+
+    println(a)
   }
+
+  SetInt(
+    """1
+      |((. X (. . .)) . (X . (. X X)))
+      |
+    """.stripMargin)
 }
--- src/main/scala/functionFractal/ws.sc
+++ src/main/scala/functionFractal/ws.sc
@@ -2,20 +2,23 @@ val s = "((. X (. . .)) . (X . (. X X)))".toList.filter(_ != ' ')
 
 trait Term
 
-case object On extends Term
+trait CellTerm extends Term
 
-case object Off extends Term
+case object On extends CellTerm
+
+case object Off extends CellTerm
 
 case class LifeTree(
-                     left: Term,
-                     node: Term,
-                     fight: Term
+                     left: Option[LifeTree] = None,
+                     node: CellTerm,
+                     right: Option[LifeTree] = None,
+                     father: Option[CellTerm] = None
                    ) extends Term
 
 
 def parsing(list: List[Char],
-            currentTerm: List[Term] = Nil
-           ): (Term, List[Char]) = {
+            currentTerm: List[LifeTree] = Nil
+           ): (LifeTree, List[Char]) = {
   list.head match {
     case '(' =>
       val (term, tail) = parsing(list.tail)
@@ -25,16 +28,31 @@ def parsing(list: List[Char],
         parsing(tail, currentTerm :+ term)
     case ')' =>
       LifeTree(
-        currentTerm(0),
-        currentTerm(1),
-        currentTerm(2)
+        Some(currentTerm(0)),
+        currentTerm(1).node,
+        Some(currentTerm(2))
       ) -> list.tail
     case 'X' =>
-      parsing(list.tail, currentTerm :+ On)
+      parsing(list.tail, currentTerm :+ LifeTree(node = On))
     case '.' =>
-      parsing(list.tail, currentTerm :+ Off)
+      parsing(list.tail, currentTerm :+ LifeTree(node = Off))
   }
 }
 
-parsing(s)
+def setFather(term: Term, father: Option[CellTerm] = None): LifeTree = {
+  term match {
+    case On          => LifeTree(node = On)
+    case Off         => LifeTree(node = On)
+    case x: LifeTree =>
+      x.copy(
+        left = x.left.map(setFather(_, Some(x.node))),
+        right = x.right.map(setFather(_, Some(x.node))),
+        father = father
+      )
+  }
+}
 
+val a =
+  parsing(s)._1
+
+setFather(a)
