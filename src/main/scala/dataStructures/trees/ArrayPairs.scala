e7a51e8dd02ac256a2aa5dcc812fa9631162aae6
yu jie shui <a1178615156@gmail.com>
2018-05-03 17:07:08 +0800

    ArrayPairs

--- src/main/scala/dataStructures/trees/ArrayPairs.scala
+++ src/main/scala/dataStructures/trees/ArrayPairs.scala
@@ -11,20 +11,20 @@ import struct.BinaryTree
 
 object ArrayPairs {
 
-  util.TreeSet
-
-
-  def canPair(num: Int, max: Int) = max / num
-
-  def solution(array: Seq[Int]) = {
-    val max = array.max
-    val (tree, sum) = array.reverse.foldLeft((null: BinaryTree.Empty[Int]) -> 0) {
-      case ((tree, sum), num) =>
-        val pair = canPair(num, max)
-        val newSum = RedBlackTree.count(RedBlackTree.to(tree, pair)) + sum
-        val newTree = RedBlackTree.update(tree, num, (), false)
-        newTree -> newSum
-    }
-    sum
-  }
+//  util.TreeSet
+//
+//
+//  def canPair(num: Int, max: Int) = max / num
+//
+//  def solution(array: Seq[Int]) = {
+//    val max = array.max
+//    val (tree, sum) = array.reverse.foldLeft((null: BinaryTree.Empty[Int]) -> 0) {
+//      case ((tree, sum), num) =>
+//        val pair = canPair(num, max)
+//        val newSum = RedBlackTree.count(RedBlackTree.to(tree, pair)) + sum
+//        val newTree = RedBlackTree.update(tree, num, (), false)
+//        newTree -> newSum
+//    }
+//    sum
+//  }
 }
--- src/test/scala/dataStructures/trees/ArrayPairsTest.scala
+++ src/test/scala/dataStructures/trees/ArrayPairsTest.scala
@@ -5,9 +5,9 @@ import org.scalatest.{FunSuite, WordSpec}
 class ArrayPairsTest extends WordSpec {
 
   "test" in {
-    val result = ArrayPairs.solution(Seq(
-      1,1,2,4,2
-    ))
-    assert(result === 8 )
+//    val result = ArrayPairs.solution(Seq(
+//      1,1,2,4,2
+//    ))
+//    assert(result === 8 )
   }
 }
